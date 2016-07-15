556d7ca9ec8236aaaabc3dd6e1cb6a229c8d175b
Erik Vullings <erik.vullings@gmail.com>
2016-07-15 23:25:10 +0200

    Added reproject option
    So I can reproject the (CBS) shape files from RD to WGS84 on the fly.
    Option -f 28992
    Closes #10

--- src/minify-geojson.ts
+++ src/minify-geojson.ts
@@ -5,6 +5,7 @@ import winston = require('winston');
 export interface ICommandOptions {
     keys: boolean,
     includeKeyMap: boolean,
+    from: string,
     topo: boolean,
     verbose: boolean,
     coordinates: number,
@@ -89,6 +90,7 @@ const optionDefinitions = [
     { name: 'keys', alias: 'k', type: Boolean, typeLabel: '[underline]{Boolean}', description: 'Minify property keys, e.g. id remains id, telephone becomes t, address a etc.' },
     { name: 'includeKeyMap', alias: 'i', type: Boolean, typeLabel: '[underline]{Boolean}', description: 'Add the key map to the GeoJSON file. Requires the -k flag too.' },
     { name: 'topo', alias: 't', type: Boolean, typeLabel: '[underline]{Boolean}', description: 'Output format is TopoJSON instead of GeoJSON' },
+    { name: 'from', alias: 'f', type: String, typeLabel: '[underline]{String}', description: 'Reproject to WGS84 by supplying the input EPSG coordinate system, e.g. -f EPSG:4326' },
     { name: 'blacklist', alias: 'b', type: String, typeLabel: '[underline]{String}', description: 'Comma separated list of properties that should be removed (others will be kept). Note that keys will not be minified unless the -k flag is used too.' },
     { name: 'whitelist', alias: 'w', type: String, typeLabel: '[underline]{String}', description: 'Comma separated list of properties that should be kept (others will be removed). Note that keys will not be minified unless the -k flag is used too.' },
     { name: 'coordinates', alias: 'c', type: Number, defaultOption: false, typeLabel: '[underline]{Positive number}', description: 'Only keep the first [italic]{n} digits of each coordinate.' },
@@ -98,9 +100,9 @@ const optionDefinitions = [
 
 
 const sections = [{
-        header: 'Minify GeoJSON',
-        content: 'Minify (compress) each input GeoJSON or ESRI shape file by replacing the attribute keys with a shorter representation (typically, its first letter). You can also reduce the number of decimals for coordinates, and whitelist and blacklist certain properties..'
-    }, {
+    header: 'Minify GeoJSON',
+    content: 'Minify (compress) each input GeoJSON or ESRI shape file by replacing the attribute keys with a shorter representation (typically, its first letter). You can also reduce the number of decimals for coordinates, and whitelist and blacklist certain properties..'
+}, {
         header: 'Options',
         optionList: optionDefinitions
     }, {
@@ -109,27 +111,30 @@ const sections = [{
             desc: '1. Shrink property keys and output to original.min.geojson',
             example: '$ minify-geojson -k original.geojson'
         }, {
-            desc: '2. A verbose version',
-            example: '$ minify-geojson -kv original.geojson'
-        }, {
-            desc: '3. Prune the blacklisted properties',
-            example: '$ minify-geojson -b "property1, property2" original.geojson'
-        }, {
-            desc: '4. Keep the whitelisted properties',
-            example: '$ minify-geojson -w "property1, property2" original.geojson'
-        }, {
-            desc: '5. Removes superfluous decimals (keep first 5)',
-            example: '$ minify-geojson -c 5 original.geojson'
-        }, {
-            desc: '6. Add the key mapping to the output',
-            example: '$ minify-geojson -ki original.geojson'
-        }, {
-            desc: '7. Convert to output to topojson (-i and -c are not used)',
-            example: '$ minify-geojson -kt original.geojson'
-        }, {
-            desc: '8. Full example',
-            example: '$ minify-geojson -ktiv -w "property1, property2" -c 5 original.geojson'
-        }]
+                desc: '2. A verbose version',
+                example: '$ minify-geojson -kv original.geojson'
+            }, {
+                desc: '3. Prune the blacklisted properties',
+                example: '$ minify-geojson -b "property1, property2" original.geojson'
+            }, {
+                desc: '4. Keep the whitelisted properties',
+                example: '$ minify-geojson -w "property1, property2" original.geojson'
+            }, {
+                desc: '5. Removes superfluous decimals (keep first 5)',
+                example: '$ minify-geojson -c 5 original.geojson'
+            }, {
+                desc: '6. Add the key mapping to the output',
+                example: '$ minify-geojson -ki original.geojson'
+            }, {
+                desc: '7. Convert output to topojson (-i and -c are not used)',
+                example: '$ minify-geojson -kt original.geojson'
+            }, {
+                desc: '8. Reproject shape file in RD (EPSG:28992) to TopoJSON',
+                example: '$ minify-geojson -ktv -f 28992 original.shp'
+            }, {
+                desc: '9. Full example',
+                example: '$ minify-geojson -ktiv -w "property1, property2" -c 5 original.geojson'
+            }]
     }
 ];
 
@@ -151,7 +156,26 @@ if (!options.src) {
 } else {
     options.src.forEach(s => {
         let file = path.isAbsolute(s) ? s : path.join(process.cwd(), s);
-        if (fs.existsSync(file)) minifyFile(file, options, () => { });
+        if (fs.existsSync(file)) {
+            loadFile(file, options, (geojson) => {
+                if (!geojson) throw new Error('Could not read input file!');
+                let ext = options.topo ? ".min.topojson" : ".min.geojson";
+                let outputFile = file.replace(/\.[^/.]+$/, ext);
+
+                if (options.from) {
+                    logger.info('REPROJECTING to WGS84');
+                    let crsName = options.from.toUpperCase();
+                    if (!crsName.match(/^EPSG:/)) crsName = `EPSG:${crsName}`;
+                    getCoordinateReferenceSystem(crsName, (crss) => {
+                        let reproject = require('reproject');
+                        geojson = reproject.toWgs84(geojson, crss, crss);                    
+                        processGeoJSON(geojson, file, outputFile, options, () => {});
+                    });
+                } else {
+                    processGeoJSON(geojson, file, outputFile, options, () => {});
+                }
+            });
+        }
     });
 }
 
@@ -160,14 +184,10 @@ if (!options.src) {
  * 
  * @param {string} inputFile
  * @param {ICommandOptions} options
- * @param {Function} done
- * @returns
+ * @param {Function} callback(GeoJSON?)
  */
-function minifyFile(inputFile: string, options: ICommandOptions, done: Function) {
-    if (!(options.keys || options.coordinates || options.whitelist || options.blacklist)) return;
-
-    let ext = options.topo ? ".min.topojson" : ".min.geojson";
-    let outputFile = inputFile.replace(/\.[^/.]+$/, ext);
+function loadFile(inputFile: string, options: ICommandOptions, cb: (geojson?: GeoJSON.FeatureCollection<GeoJSON.GeometryObject>) => void) {
+    if (!(options.keys || options.coordinates || options.whitelist || options.blacklist)) return cb();
 
     let geojson: GeoJSON.FeatureCollection<GeoJSON.GeometryObject>;
 
@@ -177,16 +197,14 @@ function minifyFile(inputFile: string, options: ICommandOptions, done: Function)
 
             geojson = JSON.parse(data);
             geojson.type = "FeatureCollection"; // this is sometimes missing
-
-            processGeoJSON(geojson, inputFile, outputFile, options, done);
+            cb(geojson);
         });
     } else if (inputFile.match(/shp$/i)) {
         let shapefile = require('shapefile');
-        shapefile.read(inputFile, (err, geojson)=> {
+        shapefile.read(inputFile, (err, geojson) => {
             if (err) throw err;
-
-            processGeoJSON(geojson, inputFile, outputFile, options, done);
-        }); 
+            cb(geojson);
+        });
     }
 }
 
@@ -207,60 +225,96 @@ function processGeoJSON(geojson: GeoJSON.FeatureCollection<GeoJSON.GeometryObjec
     if (options.whitelist) whitelist = options.whitelist.split(',').map(e => e.trim());
     if (options.blacklist) blacklist = options.blacklist.split(',').map(e => e.trim());
 
-        // Process the property keys
-        if (minifyKeys || blacklist || whitelist) {
-            geojson.features.forEach(f => {
-                if (f.properties) {
-                    if (blacklist || whitelist) f.properties = prune(f.properties, blacklist, whitelist);
-                    if (minifyKeys) f.properties = minifyPropertyKeys(f.properties);
-                }
-            });
-        }
-        // Preserver map
-        if (minifyKeys && options.includeKeyMap) {
-            geojson['map'] = reversedKeys; 
-        }
-        let json = <any> geojson;
-        // Convert to topojson
-        if (options.topo) {
-            // Overwrite the current GeoJSON object with a TopoJSON representation
-            logger.info('CONVERTING TO TOPOJSON')
-            let topojson: ITopoJSON = require("topojson");
-            let topology = topojson.topology( {collection: geojson }, { verbose: options.verbose, 'property-transform': (feature) => { return feature.properties; }});
-            topology = topojson.prune(topology, { verbose: options.verbose });
-            topology = topojson.simplify(topology, { verbose: options.verbose, 'coordinate-system': 'spherical' });
-            json = topology;
-        }
-
-        let minified: string;
-        if (!options.topo && typeof minifyCoordinates === 'number' && minifyCoordinates > 0) {
-            minified = JSON.stringify(json, (key, val) => {
-                if (isNaN(+key)) return val;
-                return val.toFixed ? Number(val.toFixed(minifyCoordinates)) : val;
-            });
-        } else {
-            minified = JSON.stringify(json);
-        }
-        fs.writeFile(outputFile, minified, (err) => {
-            if (err) throw err;
-
-            if (options.verbose) {
-                let inputStats = fs.statSync(inputFile);
-                let inputFileSizeInBytes = inputStats["size"];
-                let outputStats = fs.statSync(outputFile);
-                let outputFileSizeInBytes = outputStats["size"];
-                let percentage = 100 * (inputFileSizeInBytes - outputFileSizeInBytes) / inputFileSizeInBytes;
-                logger.info(`${inputFile} minified successfully to ${outputFile}!`);
-                if (minifyKeys) {
-                    logger.info('Key mapping:');
-                    logger.info(JSON.stringify(keys, null, 2));
-                }
-                logger.info(`Original size: ${inputFileSizeInBytes.toLocaleString('en-US', { minimumFractionDigits: 0 })}`);
-                logger.info(`New size:      ${outputFileSizeInBytes.toLocaleString('en-US', { minimumFractionDigits: 0 })}`);
-                logger.info(`Reduction:     ${percentage.toLocaleString('en-US', { minimumFractionDigits: 2 })}%`);
+    // Process the property keys
+    if (minifyKeys || blacklist || whitelist) {
+        geojson.features.forEach(f => {
+            if (f.properties) {
+                if (blacklist || whitelist) f.properties = prune(f.properties, blacklist, whitelist);
+                if (minifyKeys) f.properties = minifyPropertyKeys(f.properties);
             }
-            done();
         });
+    }
+    // Preserver map
+    if (minifyKeys && options.includeKeyMap) {
+        geojson['map'] = reversedKeys;
+    }
+    let json = <any>geojson;
+    // Convert to topojson
+    if (options.topo) {
+        // Overwrite the current GeoJSON object with a TopoJSON representation
+        logger.info('CONVERTING to TopoJSON')
+        let topojson: ITopoJSON = require("topojson");
+        let topology = topojson.topology({ collection: geojson }, { verbose: options.verbose, 'property-transform': (feature) => { return feature.properties; } });
+        topology = topojson.prune(topology, { verbose: options.verbose });
+        topology = topojson.simplify(topology, { verbose: options.verbose, 'coordinate-system': 'spherical' });
+        json = topology;
+    }
+
+    let minified: string;
+    if (!options.topo && typeof minifyCoordinates === 'number' && minifyCoordinates > 0) {
+        minified = JSON.stringify(json, (key, val) => {
+            if (isNaN(+key)) return val;
+            return val.toFixed ? Number(val.toFixed(minifyCoordinates)) : val;
+        });
+    } else {
+        minified = JSON.stringify(json);
+    }
+    fs.writeFile(outputFile, minified, (err) => {
+        if (err) throw err;
+
+        if (options.verbose) {
+            let inputStats = fs.statSync(inputFile);
+            let inputFileSizeInBytes = inputStats["size"];
+            let outputStats = fs.statSync(outputFile);
+            let outputFileSizeInBytes = outputStats["size"];
+            let percentage = 100 * (inputFileSizeInBytes - outputFileSizeInBytes) / inputFileSizeInBytes;
+            logger.info(`${inputFile} minified successfully to ${outputFile}!`);
+            if (minifyKeys) {
+                logger.info('Key mapping:');
+                logger.info(JSON.stringify(keys, null, 2));
+            }
+            logger.info(`Original size: ${inputFileSizeInBytes.toLocaleString('en-US', { minimumFractionDigits: 0 })}`);
+            logger.info(`New size:      ${outputFileSizeInBytes.toLocaleString('en-US', { minimumFractionDigits: 0 })}`);
+            logger.info(`Reduction:     ${percentage.toLocaleString('en-US', { minimumFractionDigits: 2 })}%`);
+        }
+        done();
+    });
+}
+
+/**
+ * Retrieve the CRS (Coordinate Reference System) online.
+ * 
+ * @param {string} crsName
+ * @param {Function} cb Callback function
+ * 
+ */
+function getCoordinateReferenceSystem(crsName: string, cb: (crss: Object) => void) {
+    let http = require('http');
+    let proj4 = require('proj4');
+
+    let crss = require('./crs-defs');
+    for (var k in crss) {
+        crss[k] = proj4(crss[k]);
+    } 
+
+    if (crss[crsName]) return cb(crss[crsName]);
+
+    let crsPath = crsName.toLowerCase().replace(':', '/'),
+        url = "http://www.spatialreference.org/ref/" + crsPath + "/proj4/",
+        crsDef = '';
+
+    http.get(url, function (res) {
+        if (res.statusCode != 200) {
+            throw new Error("spatialreference.org responded with HTTP " + res.statusCode +
+                " when looking up \"" + crsName + "\".");
+        }
+        res.on('data', function (chunk) {
+            crsDef += chunk;
+        }).on('end', function () {
+            crss[crsName] = proj4(crsDef);
+            cb(crss[crsName]);
+        });
+    });
 }
 
 /**
@@ -283,7 +337,7 @@ function minifyPropertyKeys(props: { [key: string]: any }) {
                 } while (reversedKeys.hasOwnProperty(replace));
             }
             keys[key] = replace;
-            reversedKeys[replace] = key; 
+            reversedKeys[replace] = key;
         }
         newProps[replace] = props[key];
     }
@@ -302,10 +356,10 @@ function smartKey(key: string) {
     if (key === id) {
         // Case 1: check for an id
         if (!reversedKeys.hasOwnProperty(id)) return id;
-    } 
+    }
     // Case 2: can we use the first letter (ignoring white space)
     let replace = key.trim()[0];
-    return reversedKeys.hasOwnProperty(replace) ? undefined : replace; 
+    return reversedKeys.hasOwnProperty(replace) ? undefined : replace;
 }
 
 /**
