f2a0ec603061275b2c54f371bdc1e92497ee01bf
Erik Vullings <erik.vullings@gmail.com>
2020-08-17 13:17:21 +0200

    Close #27 Fix regex for negative numbers

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,15 @@
 import * as fs from 'fs';
 
-export const reportLog = (logger: (message?: any, ...optionalParams: any[]) => void, inputFile: string, outputFile: string, keys?: { [key: string]: string }, count?: number) => {
+export const reportLog = (
+  logger: (message?: any, ...optionalParams: any[]) => void,
+  inputFile: string,
+  outputFile: string,
+  keys?: { [key: string]: string },
+  count?: number
+) => {
   const inputFileSizeInBytes = fs.statSync(inputFile).size;
   const outputFileSizeInBytes = fs.statSync(outputFile).size;
-  const percentage = 100 * (inputFileSizeInBytes - outputFileSizeInBytes) / inputFileSizeInBytes;
+  const percentage = (100 * (inputFileSizeInBytes - outputFileSizeInBytes)) / inputFileSizeInBytes;
   logger(`\n${inputFile}${count ? ' with ' + count + ' features' : ''} minified successfully to ${outputFile}.\n`);
   if (keys) {
     logger('Key mapping:');
@@ -32,14 +38,21 @@ export type FilterOperatorType = '=' | '<' | '>' | '<=' | '>=' | '!=';
 export const createPropertyFilter = (prop: string, op: FilterOperatorType, val: string | number) => {
   switch (op) {
     // tslint:disable-next-line:triple-equals
-    case '=': return (props: Object) => props.hasOwnProperty(prop) && props[prop] == val;
-    case '<': return (props: Object) => props.hasOwnProperty(prop) && props[prop] < val;
-    case '>': return (props: Object) => props.hasOwnProperty(prop) && props[prop] > val;
-    case '<=': return (props: Object) => props.hasOwnProperty(prop) && props[prop] <= val;
-    case '>=': return (props: Object) => props.hasOwnProperty(prop) && props[prop] >= val;
+    case '=':
+      return (props: Object) => props.hasOwnProperty(prop) && props[prop] == val;
+    case '<':
+      return (props: Object) => props.hasOwnProperty(prop) && props[prop] < val;
+    case '>':
+      return (props: Object) => props.hasOwnProperty(prop) && props[prop] > val;
+    case '<=':
+      return (props: Object) => props.hasOwnProperty(prop) && props[prop] <= val;
+    case '>=':
+      return (props: Object) => props.hasOwnProperty(prop) && props[prop] >= val;
     // tslint:disable-next-line:triple-equals
-    case '!=': return (props: Object) => props.hasOwnProperty(prop) && props[prop] != val;
-    default: throw new Error(`Operator ${op} is not supported!`);
+    case '!=':
+      return (props: Object) => props.hasOwnProperty(prop) && props[prop] != val;
+    default:
+      throw new Error(`Operator ${op} is not supported!`);
   }
 };
 
@@ -49,14 +62,15 @@ export const createPropertyFilter = (prop: string, op: FilterOperatorType, val: 
  * @param {string} filterQuery
  */
 export const convertQueryToPropertyFilters = (filterQuery: string): Array<(props: Object) => boolean> => {
-  const re = /^([a-zA-Z_ 0-9]*) ?([!<>=]{1,2}) ?([a-zA-Z_ -0-9]*)$/;
+  const re = /^([a-zA-Z_\-0-9]*) *([!<>=]{1,2}) *([a-zA-Z_\-0-9]*)$/;
 
-  const queries = filterQuery.split(',').map(q => q.trim());
+  const queries = filterQuery.split(',').map((q) => q.trim());
   const filters = [];
-  queries.forEach(q => {
+  queries.forEach((q) => {
     const m = re.exec(q);
     if (!m || m.length !== 4) {
-      const errMsg = 'Filters should be in the form PROPERTY OPERATOR VALUE, where property is a string, OPERATOR is <, =, >, !=, <= or >=, and VALUE is a string or number.';
+      const errMsg =
+        'Filters should be in the form PROPERTY OPERATOR VALUE, where property is a string, OPERATOR is <, =, >, !=, <= or >=, and VALUE is a string or number.';
       console.error(errMsg);
       throw new Error(errMsg);
     }
